2b5a2321af5b4df8e71fa6d536bcd60c1f3344ec
Pablo Sierra <pavels.jo@gmail.com>
2023-10-16 17:32:30 -0300

    Minus fix

--- js/form.js
+++ js/form.js
@@ -13,5 +13,5 @@ form.addEventListener('submit', (e) => {
     .then(data => {
         console.log(data)});
         form.reset();
-        M.toast({ html: '<h1>Gracias por confirmanos tu asistenciad.</h1>.'});
+        M.toast({ html: '<h2>Gracias por confirmanos tu asistencia.</h2>.'});
 });
