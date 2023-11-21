70c570814d21703810aae51955b91a6c9c567c0c
Pablo Sierra <pavels.jo@gmail.com>
2023-11-21 15:04:55 -0300

    Form bug fixed

--- js/form.js
+++ js/form.js
@@ -2,15 +2,9 @@ const form = document.querySelector("form");
 const formSection = document.getElementById('form-asistencia');
 const positiveResponseSection = document.getElementById('form-asistencia-positiva');
 const negativeResponseSection = document.getElementById('form-asistencia-negativa');
-const buttonEnviar = document.getElementById('boton-enviar');
 const buttonCerrarFormaPositiva = document.getElementById('boton-cerrar-forma-positiva');
 const buttonCerrar = document.getElementById('boton-cerrar');
 const loader = document.getElementById('loader');
-
-buttonEnviar.addEventListener('click', (e) => {
-    buttonEnviar.classList.toggle('disabled');
-    loader.style.display = 'block';
-});
 
 buttonCerrarFormaPositiva.addEventListener('click', (e) => {
 
@@ -29,6 +23,10 @@ buttonCerrar.addEventListener('click', (e) => {
 form.addEventListener('submit', (e) => {
     
     e.preventDefault();
+
+    buttonEnviar.classList.toggle('disabled');
+    loader.style.display = 'block';
+
     const data = new FormData(form);
     const respuesta = data.get('assist');
 
