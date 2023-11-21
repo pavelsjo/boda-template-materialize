914a4933047a0af189fd62cc4c3159f6dc1c35e7
Pablo Sierra <pavels.jo@gmail.com>
2023-11-21 11:44:27 -0300

    New form UIUX

--- js/form.js
+++ js/form.js
@@ -1,17 +1,48 @@
-let form = document.querySelector("form");
+const form = document.querySelector("form");
+const formSection = document.getElementById('form-asistencia');
+const positiveResponseSection = document.getElementById('form-asistencia-positiva');
+const negativeResponseSection = document.getElementById('form-asistencia-negativa');
+const buttonAgregar = document.getElementById('boton-agregar');
+const buttonCerrar = document.getElementById('boton-cerrar');
+
+
+buttonAgregar.addEventListener('click', (e) => {
+
+    formSection.style.display = 'block';
+    positiveResponseSection.style.display = 'none';
+
+});
+
+buttonCerrar.addEventListener('click', (e) => {
+
+    formSection.style.display = 'block';
+    negativeResponseSection.style.display = 'none';
+
+});
 
 form.addEventListener('submit', (e) => {
     
     e.preventDefault();
-    let data = new FormData(form);
-    console.log(data)
+    const data = new FormData(form);
+    const respuesta = data.get('assist');
+
     fetch('https://script.google.com/macros/s/AKfycbzhnkwvExa6xoIDSSD-rQZKweGu-AT2VvIf0J8tKLro4V3_e0760f6ZrInLvqVjBqgN/exec', {
         method: "POST",
         body: data
     })
     .then(res => res.text())
     .then(data => {
-        console.log(data)});
-        form.reset();
-        M.toast({ html: '<h2>Gracias por confirmanos tu asistencia.</h2>.'});
-});
+        if (data === "Success") {
+
+            form.reset();
+            formSection.style.display = 'none';
+        
+            if(respuesta === "Sí") {
+                 positiveResponseSection.style.display = 'block';
+                } if(respuesta=== "No") {
+                    negativeResponseSection.style.display = 'block';
+                }
+            }
+    });
+
+})
